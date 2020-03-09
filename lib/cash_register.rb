2aefeb97c0197dcce71d09810c83042714f4ab41
Alexia Adediran <alexiaadediran30@gmail.com>
2020-03-09 01:14:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,122 +1,40 @@
-# require 'pry'
-# class CashRegister
-#   attr_accessor :total, :discount, :price, :items
-
-#   def initialize(discount = 0)
-#     @total = 0
-#     @discount = discount
-#     @items = []
-#   end
-
-#   def add_item(item, price, quantity = 1)
-#     @price = price
-#     @total += price * quantity
-#     if quantity > 1
-#       counter = 0
-#       while counter < quantity
-#         @items << item
-#         counter += 1
-#       end
-#     else
-#       @items << item
-#     end
-#   end
-
-#   def apply_discount
-#     if @discount > 0
-#       @to_take_off = (price * discount)/100
-#       @total -= @to_take_off
-#       return "After the discount, the total comes to $#{total}."
-#     else
-#       return "There is no discount to apply."
-#     end
-#   end
-
-#   def void_last_transaction
-#     @total -= @price
-#   end
-
-# end
-#   require 'pry'
-#   class CashRegister
-
-#   attr_accessor :total, :discount, :last_transaction_amount, :items
-
-#   def initialize(discount = 0)
-#     @total = 0
-#     @discount = discount
-#     @items = []
-#   end
-
-
-#   def add_item(title,price,quantity = 1)
-#     if quantity>1
-#       i=0
-#       while i<quantity
-#         @items << title
-#         i+=1
-#       end
-#     else
-#       @items << title
-#     end
-#     @total += price*quantity
-#     @last_transaction_amount = @total
-#     @total
-#   end
-
-#   def apply_discount()
-#     if @discount > 0
-#       @discount = @discount/100.to_f
-#       @total = @total - (@total * (@discount))
-#       "After the discount, the total comes to $#{@total.to_i}."
-#     else
-#       "There is no discount to apply."
-#     end
-#   end
-
-
-#   def void_last_transaction()
-#     @total -= @last_transaction_amount
-#   end
-
-# end
-class CashRegister 
-  attr_accessor :discount, :item, :last_transaction, :total
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :price, :items
 
   def initialize(discount = 0)
-    @items = []
+    @total = 0
     @discount = discount
-    @total = 0 
-  end 
-  
-  def add_item(cost, name, number = 1)
-    self.total = cost * number
-    @items << name
+    @items = []
   end
-  
-  def add_item_2(cost, name, number = 1)
-    self.total = cost * number
 
-  def apply_discount 
-  # the cash register was initialized with an employee discount:  
-    # applies the discount to the total price
-    # returns success message with updated total
-    # reduces the total
-    
-  # the cash register was *NOT* initialized with an employee discount:
-    # returns a string error message that there is no discount to appply
-  end 
-  
-  def items 
-  # (self).items!?
-  # returns an array containing all items that have been added
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
   end
-  
+
+  def apply_discount
+    if @discount > 0
+      @to_take_off = (price * discount)/100
+      @total -= @to_take_off
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
   def void_last_transaction
-  # subtracts the last transaction from the total 
-  end 
-  
-end 
+    @total -= @price
+  end
 
-tester = CashRegister.new 
-tester.add_item(4, "carrots", 4)
+end
+#  
